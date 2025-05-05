e5d6de7428f20aa0ddda8659a29770d6e1dcdc44
Rahul Bisht <rahulbisht8886@gmail.com>
2025-05-05 19:58:45 +0530

    added logo

--- frontend/src/Component/AetherSignup.jsx
+++ frontend/src/Component/AetherSignup.jsx
@@ -1,25 +1,22 @@
-import React from 'react';
+import React from "react";
 
 const AetherSignup = () => {
   // Option 1: If SVG is in public/
-  const circle = "/circle.svg"; 
+  const circle = "/circle.svg";
 
   // Option 2: If SVG is in src/assets/
   // import circle from './assets/circle.svg';
 
   return (
     <div className="bg-[#060713] h-screen w-full">
-      <div className="w-full border-2 border-white text-white p-4">
-        <h1
-          className="
-            tracking-widest font-extrabold text-4xl 
-            bg-[url('/circle.svg')] bg-cover 
-            bg-clip-text text-transparent
-            [-webkit-text-fill-color:transparent]
-            bg-blend-overlay
-          "
-        >
-          AETHER.
+      <div className="w-full text-white p-4">
+        <h1>
+          <strong
+            className="tracking-tighter font-black text-5xl 
+            bg-[url('/circle.svg')] inline-block bg-bottom bg-[length:500px] text-transparent bg-clip-text [-webkit-text-fill-color:transparent] bg-blend-overlay"
+          >
+            AETHER.
+          </strong>
         </h1>
       </div>
     </div>
