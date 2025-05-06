1b46327bd92f3fef864cef2b644624f86c9103c9
Aman <bhaskarshah4235@gmail.com>
2025-05-06 23:22:29 +0530

    Main page modified

--- frontend/src/pages/(Auth-Pages)/Main.jsx
+++ frontend/src/pages/(Auth-Pages)/Main.jsx
@@ -1,9 +1,37 @@
-import React from 'react'
+import React from "react";
+import AehterLogo from "../../Component/AehterLogo";
+import { FaUsers } from "react-icons/fa";
 
 const Main = () => {
   return (
-    <div>Main</div>
-  )
-}
+    <div className=" text-white logo h-screen w-full flex flex-col items-center bg-black border border-white py-5">
+      <AehterLogo />
+      <div className="border border-white ">
+        <div className="div-for-addFriends">
+          <h2 className="text-center">Add Friends</h2>
+          <div className="bg-gray-800 flex gap-3 border-3 rounded-md border-slate-900 px-2 py-1">
+            <input
+              type="text"
+              className=""
+              placeholder="You can send friend request with their aether's username"
+            />
+            <button className="cursor-pointer bg-blue-950 px-1 py-1 rounded-md">
+              Send Friend Req
+            </button>
+          </div>
+        </div>
+        <hr />
+        <div className="divForNotifications">
+          <h2 className="text-center">Notifications</h2>
+          <div className="">/notifications</div>
+        </div>
+      </div>
 
-export default Main
+      <div className="sticky-navBar ">
+        
+      </div>
+    </div>
+  );
+};
+
+export default Main;
