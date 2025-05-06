cce36fc044c8ceb971c09f6ab74f0ed581b96c90
Rahul Bisht <rahulbisht8886@gmail.com>
2025-05-06 21:11:12 +0530

    created main

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 // import SignIn from './pages/SignIn'
-import Home from './pages/Home'
+// import Home from './pages/(Auth-Pages)/Home'
+import Main from './pages/(Auth-Pages)/Main'
 const App = () => {
   return (
-    <Home/>
+    // <Home/>
     // <SignIn/>
+    <Main/>
   )
 }
 
---
+++ frontend/src/pages/(Auth-Pages)/Home.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+import AehterLogo from "../../Component/AehterLogo";
+
+const Home = () => {
+  return (
+    <div className="bg-[#060713] h-screen w-full flex justify-center items-center text-white">
+      <div className="main-div  flex justify-center items-center px-15 py-28">
+        <div className="p-4  flex flex-col gap-6 justify-center items-center">
+          <AehterLogo />
+          <button className="border-none bg-green-700 rounded-lg cursor-pointer font-semibold w-full hover:bg-green-800 py-1">
+            Login
+          </button>
+          <button className="border-none bg-gray-700 rounded-lg cursor-pointer font-semibold w-full hover:bg-gray-800 py-1">
+            Guest
+          </button>
+          <p className="text-sm tracking-tighter">
+            Don't have account ?{" "}
+            <a href="" className="text-blue-600 hover:text-blue-800">
+              create one
+            </a>
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Home;
