e0acb95d3af8a12107467cd6b830828d5232c8b5
Rahul Bisht <rahulbisht8886@gmail.com>
2025-05-06 17:47:06 +0530

    add

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -5,8 +5,8 @@ const Home = () => {
 
   return (
     <div className="bg-[#060713] h-screen w-full flex justify-center items-center text-white">
-      <div className="main-div border border-red-800 flex justify-center items-center px-15 py-28">
-        <div className="p-4 border border-gray-700 flex flex-col gap-6 justify-center items-center">
+      <div className="main-div  flex justify-center items-center px-15 py-28">
+        <div className="p-4  flex flex-col gap-6 justify-center items-center">
          <AehterLogo/> 
           <button className="border-none bg-green-700 rounded-lg cursor-pointer font-semibold w-full hover:bg-green-800 py-1">Login</button>
           <button className="border-none bg-gray-700 rounded-lg cursor-pointer font-semibold w-full hover:bg-gray-800 py-1">Guest</button>
